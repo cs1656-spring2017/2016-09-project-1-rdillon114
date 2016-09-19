2aa3b1dc7ef2f946bcd36e64656dae72b472b376
rdillon114 <rad114@pitt.edu>
2016-09-19 00:32:08 -0400

    The inverted index data structure is completed.  However, I still need to remove code that was used for debugging.

--- ire-index.py
+++ ire-index.py
@@ -1,10 +1,8 @@
 import os
+import nltk
+import json
 
-print os.path
 documents = {}
-l = 'abc@e#g'
-l = l.translate(None, '!@#$')
-print l
 
 for filename in os.listdir(os.path.join("input/")):
     f = open('input/' + filename)
@@ -44,13 +42,75 @@ for key in documents:#for every key in documents, we get the lines, and split th
 
     documents[key] = lines
 
+porter = nltk.PorterStemmer()
+
+#time to apply stemming to all words in all lines in all documents.
+for key in documents:
+    lines = documents[key]
+    for key1 in lines:
+        words = lines[key1].split(" ")
+        i = 0
+        str = ""
+        for word in words:
+            if i == 0:
+                str = porter.stem(word)
+                i += 1
+            else:
+                str = str + " " + porter.stem(word)
+        lines[key1] = str
+        #next two lines are for debugging only.
+        words = lines[key1]
+        print words
+
+#create an inverted index of word counts in documents. first create a list of distinct words, then create x amount of
+#dictionaries where x is the number of documents in input/ and initialize all of them to have counts of 0 for each
+#distinct word in the documents.
+
+#I am creating the inverted index by making a dictionary of documents that holds dictionaries of words.  To do this,
+#I must first make a list of all unique words over all documents.
+tempDict = {}
+
 for key in documents:
     lines = documents[key]
     for key1 in lines:
         words = lines[key1].split(" ")
         for word in words:
-            print word
+            tempDict[word] = 0
 
-#create an inverted index of word counts in documents. first create a list of distinct words, then create x amount of
-#dictionaries where x is the number of documents in input/ and initialize all of them to have counts of 0 for each
-#distinct word in the documents.
+#now using this temp dictionary, we can initialize a dictionary of words for each document.
+#docs is the dictionary of dictionaries that will hold the word counts for all documents.
+docs = {}
+
+for key in documents:
+    docs[key] = {}
+
+#initializing the word count dictionaries for each document to have a count of 0 for every unique word over all the documents.
+for key in docs:
+    for word in tempDict:
+        docs[key][word] = 0
+
+for key in documents:
+    lines = documents[key]
+    for key1 in lines:
+        words = lines[key1].split(" ")
+        for word in words:
+            docs[key][word] += 1
+
+
+#this is for debugging only..... looking at the counts of each word in each document.
+for key in docs:
+    for word in docs[key]:
+        print docs[key][word]
+
+#outputting the docs dictionary to the json file.
+file = open("inverted-index.json", "wb")
+json.dump(docs, file)
+file.close()
+
+
+'''
+print '\n\n\n\n'
+for key in docs:
+    for key1 in docs[key]:
+        print key1
+        print docs[key][key1]'''
